d5e1c5d3bc6aa51873a5b9cff46a8e27dc17b7cb
Héctor Romero López <hector@hromerol.es>
2022-11-25 10:13:52 +0100

    .

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ r = requests.post("http://127.0.0.1:8000/person",data={
     "email":"ased"
 })
 print(r.status_code)
-if r.status_code != 201 or r.status_code != 200:
+if int(r.status_code) != 201 or int(r.status_code) != 200:
     exit(1)
 exit(0)
