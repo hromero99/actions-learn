7fed3b546c1ecc1440f47af8224138de5830dd95
Héctor Romero López <hector@hromerol.es>
2022-11-25 10:34:01 +0100

    .

--- test2.py
+++ test2.py
@@ -1,9 +1,7 @@
 import requests
 
 status_code = 1
-r = requests.post("http://127.0.0.1:8000/person",data={
-    
-})
+r = requests.post("http://127.0.0.1:8000/test",data={})
 print(r.status_code)
 if int(r.status_code) == 200 or int(r.status_code) == 201:
     status_code = 0
